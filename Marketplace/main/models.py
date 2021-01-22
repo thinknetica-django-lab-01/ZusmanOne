ce7084883199cf65fa5c9189f398827fecee5ac6
ZusmanOne <bigmama93@mail.ru>
2021-01-22 20:50:54 +0300

    add_4model

--- Marketplace/main/models.py
+++ Marketplace/main/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description_product = models.TextField(blank=True, help_text='введите описание товара')
     price_product = models.DecimalField(max_digit=10, decimal_places=10, help_text='укажите цену')
     created_product = models.DateTimeField(auto_now_add=True)
-    saleman_product = models.ForeigKey('SaleMan', null=True, on_delte=models.SET_NULL)
+    saleman_product = models.ForeignKey('SaleMan', null=True, on_delte=models.SET_NULL)
 
 
 class SaleMan(models.Model):
@@ -14,4 +14,11 @@ class SaleMan(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     e_mail = models.EmailField(max_length=100)
+
+class CategoryProduct(models.Model):
+    title_category = models.CharField(max_length=100,help_text='введите категорию товара')
+
+class TagProduct(models.Model):
+    title_tag = models.CharField(max_length=100)
+
 # Create your models here.
