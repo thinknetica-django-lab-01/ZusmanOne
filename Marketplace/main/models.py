4cdeb8535da05bc866137d5af9ebc4402fcaad46
ZusmanOne <bigmama93@mail.ru>
2021-01-22 18:37:40 +0300

    create_2model

--- Marketplace/main/models.py
+++ Marketplace/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+class Product(models.Model):
+    title_product = models.CharField(max_length=100, help_text='введите название продукта')
+    description_product = models.TextField(blank=True, help_text='введите описание товара')
+    price_product = models.DecimalField(max_digit=10, decimal_places=10, help_text='укажите цену')
+    created_product = models.DateTimeField(auto_now_add=True)
+    saleman_product = models.ForeigKey('SaleMan', null=True, on_delte=models.SET_NULL)
+
+
+class SaleMan(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    e_mail = models.EmailField(max_length=100)
 # Create your models here.
