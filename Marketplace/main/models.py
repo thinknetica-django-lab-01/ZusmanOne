4dd37d53434b1f329e1a4cb4674d8a16a1e3de3d
ZusmanOne <bigmama93@mail.ru>
2021-01-23 18:41:47 +0300

    edit_model

--- Marketplace/main/models.py
+++ Marketplace/main/models.py
@@ -6,9 +6,16 @@ class Product(models.Model):
     description_product = models.TextField(blank=True, help_text='введите описание товара')
     price_product = models.DecimalField(max_digits=10, decimal_places=10, help_text='укажите цену')
     created_product = models.DateTimeField(auto_now_add=True)
-    saleman_product = models.ForeignKey('SaleMan', on_delete=models.SET_NULL, null=True, related_name='saleman_product')
+    saleman_product = models.ForeignKey('SaleMan', on_delete=models.CASCADE, null=True, related_name='saleman_product')
     tag_product = models.ManyToManyField('TagProduct', related_name='tag_product')
     category_product = models.ForeignKey('CategoryProduct', null=True, blank=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.title_product
 
 
 class SaleMan(models.Model):
@@ -17,10 +24,29 @@ class SaleMan(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     e_mail = models.EmailField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Продавец'
+
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
 class CategoryProduct(models.Model):
     title_category = models.CharField(max_length=100,help_text='введите категорию товара')
 
+    class Meta:
+        verbose_name = 'Категория'
+
+    def __str__(self):
+        return self.title_category
+
 class TagProduct(models.Model):
     title_tag = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Тэги '
+
+    def __str__(self):
+        return self.title_tag
 
 # Create your models here.
