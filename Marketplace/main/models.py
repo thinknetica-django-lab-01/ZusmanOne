a7d04ac83335bb3405f26513ec753d77de49262e
ZusmanOne <bigmama93@mail.ru>
2021-01-23 15:55:37 +0300

    make_migrations

--- Marketplace/main/models.py
+++ Marketplace/main/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class Product(models.Model):
     title_product = models.CharField(max_length=100, help_text='введите название продукта')
     description_product = models.TextField(blank=True, help_text='введите описание товара')
-    price_product = models.DecimalField(max_digit=10, decimal_places=10, help_text='укажите цену')
+    price_product = models.DecimalField(max_digits=10, decimal_places=10, help_text='укажите цену')
     created_product = models.DateTimeField(auto_now_add=True)
-    saleman_product = models.ForeignKey('SaleMan', null=True, on_delte=models.SET_NULL, related_name='saleman_product')
+    saleman_product = models.ForeignKey('SaleMan', on_delete=models.SET_NULL, null=True, related_name='saleman_product')
     tag_product = models.ManyToManyField('TagProduct', related_name='tag_product')
+    category_product = models.ForeignKey('CategoryProduct', null=True, blank=True, on_delete=models.SET_NULL)
 
 
 class SaleMan(models.Model):
