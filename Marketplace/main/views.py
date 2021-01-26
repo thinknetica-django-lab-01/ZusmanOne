b9765dcac2642661d4e8015fabf9143df09f5ba7
ZusmanOne <bigmama93@mail.ru>
2021-01-26 22:40:25 +0300

    make_nameuser_and_turnonblock

--- Marketplace/main/views.py
+++ Marketplace/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    turn_on_block = False
+    return render(request, 'index.html', {'turn_on_block': turn_on_block})
+
+
 
 # Create your views here.
