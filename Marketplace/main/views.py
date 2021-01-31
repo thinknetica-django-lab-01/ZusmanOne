15f9ea65063562be5a65f989b48b6b89228b8d8e
ZusmanOne <bigmama93@mail.ru>
2021-02-01 02:04:19 +0300

    'tag_done'

--- Marketplace/main/views.py
+++ Marketplace/main/views.py
@@ -19,6 +19,22 @@ class ProductListView(ListView):
     context_object_name = 'my_product'
     template_name = "main/product_list.html"
     paginate_by = 3
+
+    def get_queryset(self, **kwargs):
+        tag = self.request.GET.get('tag')
+        if tag:
+            return Product.objects.filter(tag_product__title_tag=tag)
+        return super().get_queryset(**kwargs)
+
+    def context_data(self, **kwargs):
+        context = super(self).get_context_data(**kwargs)
+        my_tag = self.request.GET.get('tag')
+        if my_tag:
+            context['tag'] = my_tag
+        return context
+
+
+
     # def get_context_data(self, **kwargs):
     #     context = super(ProductListView, self).get_context_data(**kwargs)
     #     if not context.get('is_paginated', False):
