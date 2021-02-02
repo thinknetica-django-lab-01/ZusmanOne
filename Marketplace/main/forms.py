b5136a9a037a790b845bc29e6b685985de54f997
ZusmanOne <bigmama93@mail.ru>
2021-02-02 13:07:09 +0300

    validation_done

--- Marketplace/main/forms.py
+++ Marketplace/main/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class UpdateProfile(forms.ModelForm):
+    age=forms.IntegerField()
+
+    def clean_age(self):
+        if self.cleaned_data['age']<18:
+            raise ValidationError('Возвращайтесь когда вам исполнится 18 лет')
+        return self.cleaned_data['age']
 
     class Meta:
         model = User
