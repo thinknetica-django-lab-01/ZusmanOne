d21bed0d510d575753fbf032c214fab3d910412e
ZusmanOne <bigmama93@mail.ru>
2021-02-05 23:44:28 +0300

    secret_key2

--- Marketplace/Marketplace/settings.py
+++ Marketplace/Marketplace/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(fxtxp_^e&s!wbwrxxsxf3e9o5!+ioy)m1x@-9mwe2j@($ni#m'
+SECRET_KEY = config.('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
